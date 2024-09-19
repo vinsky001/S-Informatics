f244568c21e2da05641a51cde87209c357cd401a
vinsky001 <ekibet544@gmail.com>
2024-09-19 19:53:21 +0300

    Feature: Added `item field` to the database

--- core/models.py
+++ core/models.py
@@ -20,6 +20,7 @@ class Order(models.Model):
     order_date = models.DateField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     time = models.DateTimeField(auto_now_add=True)
+    item = models.CharField(max_length=50)
     
     def __str__(self):
         return f"Order {self.id} by {self.customer.name}"
