5e97e81d58f7f85f9eac21fa561e15daea853cac
vinsky001 <ekibet544@gmail.com>
2024-09-22 12:14:05 +0300

    Added path to template as home

--- server/urls.py
+++ server/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include("core.urls")),
+    path('home/', TemplateView.as_view(template_name='dashboard/home.html'), name='home'),
     path('accounts/', include('allauth.urls')),
          
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
